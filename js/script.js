22a6321ac3a59d6d063e355e89d0314b44bd3e5d
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2022-12-26 17:30:50 -0300

    add parametros via url para orçamento

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Ativar links do menu
+
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -10,3 +12,18 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar Items do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+console.log(parametros);
